a0c8ff1d4b813d0ce99ad81dcf4ce3fb792d0fba
Web Developer <username@Lubuntu-VirtualBox.(none)>
2012-11-07 23:22:55 -0700

    Soccer Jewelry

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '4df2f87a1a8e4a669df376fa9e88b0a2de0f71dc1a71379a3b3a7d4067689772face13937a27c39dcbb75e92389e24689ea2464acc9596cac03f5de3227d30ce'
